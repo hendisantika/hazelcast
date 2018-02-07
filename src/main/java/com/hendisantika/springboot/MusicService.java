00d60bb64540b0c0cf778c233ef1f8843af2f574
Hendi Santika <hendisantika@gmail.com>
2018-02-07 07:11:18 +0700

    #4 - Add Music Service

---
+++ src/main/java/com/hendisantika/springboot/MusicService.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+@CacheConfig(cacheNames = "instruments")
+public class MusicService {
+
+    private static Logger log = LoggerFactory.getLogger(MusicService.class);
+
+    @CacheEvict(allEntries = true)
+    public void clearCache() {
+    }
+
+    @Cacheable(condition = "#instrument.equals('trombone')")
+    public String play(String instrument) {
+        log.info("Executing: " + this.getClass().getSimpleName() + ".play(\"" + instrument + "\");");
+        return "paying " + instrument + "!";
+    }
+
+}
